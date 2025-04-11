9851dff6decaac8e126727eda4347978569aba31
Jerry Bruwes <jbruwes@icloud.com>
2025-04-11 17:44:24 +0200

    modified:   src/boot/i18n.ts

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,20 +1,19 @@
-import { defineBoot } from '#q-app/wrappers';
-import { createI18n } from 'vue-i18n';
-
-import messages from 'src/i18n';
+import { defineBoot } from "#q-app/wrappers";
+import messages from "src/i18n";
+import { createI18n } from "vue-i18n";
 
 export type MessageLanguages = keyof typeof messages;
 // Type-define 'en-US' as the master schema for the resource
-export type MessageSchema = typeof messages['en-US'];
+export type MessageSchema = (typeof messages)["en-US"];
 
 // See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-declare module 'vue-i18n' {
-  // define the locale messages schema
-  export interface DefineLocaleMessage extends MessageSchema {}
-
+declare module "vue-i18n" {
   // define the datetime format schema
   export interface DefineDateTimeFormat {}
+
+  // define the locale messages schema
+  export interface DefineLocaleMessage extends MessageSchema {}
 
   // define the number format schema
   export interface DefineNumberFormat {}
@@ -22,9 +21,9 @@ declare module 'vue-i18n' {
 /* eslint-enable @typescript-eslint/no-empty-object-type */
 
 export default defineBoot(({ app }) => {
-  const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
-    locale: 'en-US',
+  const i18n = createI18n<{ message: MessageSchema }>({
     legacy: false,
+    locale: "en-US",
     messages,
   });
 
