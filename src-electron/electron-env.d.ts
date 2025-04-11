dc4014aa1bf9d7277ab7994551c284b594ec9005
Jerry Bruwes <jbruwes@icloud.com>
2025-04-11 17:46:38 +0200

    modified:   src-electron/electron-env.d.ts

--- src-electron/electron-env.d.ts
+++ src-electron/electron-env.d.ts
@@ -1,8 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    QUASAR_PUBLIC_FOLDER: string;
-    QUASAR_ELECTRON_PRELOAD_FOLDER: string;
-    QUASAR_ELECTRON_PRELOAD_EXTENSION: string;
     APP_URL: string;
+    QUASAR_ELECTRON_PRELOAD_EXTENSION: string;
+    QUASAR_ELECTRON_PRELOAD_FOLDER: string;
+    QUASAR_PUBLIC_FOLDER: string;
   }
 }
